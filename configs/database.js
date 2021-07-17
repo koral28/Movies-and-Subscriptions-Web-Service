28ea7a820b9abd5e3caafc975810d19018233d27
Koral <koral.levi22@gmail.com>
2021-07-17 13:43:21 +0300

    fix bugs

--- configs/database.js
+++ configs/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 mongoose
-  .connect("mongodb://localhost:27017/SubscriptionsDB", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
-
- 
